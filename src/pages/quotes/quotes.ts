6a169716265783c566788ce8c597bc97b675a292
danielyaakove <danielyaakove@gmail.com>
2017-05-08 23:03:33 +0300

    build quote page

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Category } from '../../data/category.interface';
+import { Quote } from '../../data/quote.interface'
 
 @IonicPage()
 @Component({
@@ -19,6 +20,10 @@ export class Quotes implements OnInit {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Quotes');
+  }
+
+  onAddToFavorite(quote:Quote){
+
   }
 
 }
